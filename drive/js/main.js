858d4f429eb8ea1e5c45aa9981b97432cb7b9b08
Onik Quddus <Onik Quddus>
2016-11-20 15:27:19 -0500

    online registration new form and enter bug fix

--- drive/js/main.js
+++ drive/js/main.js
@@ -120,7 +120,12 @@ $(document).ready(function(){
       changeYear: true
     });
     
-
+    //Prevent submission by hitting enter
+    $(window).keydown(function(event){
+        if(event.keyCode == 13) {
+          event.preventDefault();
+          return false;
+        }});
     
 
 	//Hidden Steps
--- js/main.js
+++ js/main.js
@@ -118,6 +118,13 @@ $(document).ready(function(){
       changeMonth: true,
       changeYear: true
     });
+    
+    //Prevent submission by hitting enter
+    $(window).keydown(function(event){
+        if(event.keyCode == 13) {
+          event.preventDefault();
+          return false;
+        }});
 	
 	//Hidden Steps
 	$('#step2').hide();
